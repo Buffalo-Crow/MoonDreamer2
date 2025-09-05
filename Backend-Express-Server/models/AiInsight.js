cbbf4e955e69cd965bf88623adc46cc6f342d681
crowwalk <crowwalk@icloud.com>
2025-09-04 21:28:41 -0400

    update Schemas and add AI Schema

---
+++ Backend-Express-Server/models/AiInsight.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const AiInsightSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+
+  dreamIds: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Dream",
+      required: true,
+    },
+  ],
+
+  summary: {
+    type: String,
+    required: true,
+  },
+
+  tags: [String],
+
+  scope: {
+    type: String,
+    enum: ["single", "user-pattern", "communtity-pattern"],
+    default: "single",
+  },
+
+  model: {
+    type: String,
+    enum: ["gpt-3.5-turbo", "gpt-4"],
+    default: "gpt-3.5-turbo",
+  },
+
+  version: {
+    type: Number,
+    default: 1,
+  },
+
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("AiInsight", AiInsightSchema);
